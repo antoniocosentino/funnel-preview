dcbb74f0e99fc68df7970e546b62474d4994b7a5
Antonio Cosentino <hello@antoniocosentino.it>
2025-02-09 14:43:52 +0100

    simplified device frame

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,12 +24,8 @@ export default function Home() {
                     <button className="text-gray-600 hover:text-gray-900 text-3xl">&#8592;</button>
 
                     {/* Mock Device */}
-                    <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px]">
-                        <div className="h-[32px] w-[3px] bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
-                        <div className="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-                        <div className="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-                        <div className="h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-                        <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800">
+                    <div className="relative mx-auto border-gray-50 bg-white border-[14px] rounded-[2.5rem] h-[748px] w-[403px] shadow-lg">
+                        <div className="rounded-[2rem] overflow-hidden w-[375px] h-[720px] bg-white dark:bg-gray-800">
                             <div className="mt-2 ml-2 text-lg font-semibold">Hello World</div>
                         </div>
                     </div>
