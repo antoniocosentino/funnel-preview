3f59cd66768285acecdf4b2ec46fbe7c0acd96a1
Antonio Cosentino <hello@antoniocosentino.it>
2025-02-10 17:00:56 +0100

    button block

--- src/components/DeviceFrame.tsx
+++ src/components/DeviceFrame.tsx
@@ -6,11 +6,11 @@ type DeviceFrameProps = {
 
 const DeviceFrame: React.FC<DeviceFrameProps> = ({ children }) => (
     <div className="relative mx-auto border-gray-200 bg-white border-[14px] rounded-[2.5rem] h-[748px] w-[403px] shadow-sm">
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4 w-[120px] h-[30px] rounded-[15px] bg-gray-200">
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4 w-[120px] h-[30px] rounded-[15px] bg-gray-200 z-10">
             <div className="absolute top-2.5 right-2 transform -translate-x-1/2 w-[10px] h-[10px] rounded-[10px] bg-white z-10"></div>
         </div>
-        <div className="rounded-[2rem] overflow-hidden w-[375px] h-[690px] bg-white mt-8">
-            <div className="pt-7 text-lg font-semibold">
+        <div className="rounded-br-[2rem] rounded-bl-[2rem] w-[375px] h-[690px] bg-white mt-8 relative">
+            <div className="text-lg font-semibold absolute bg-red-300 top-6 overflow-x-scroll scrollbar-hidden rounded-br-[2rem] rounded-bl-[2rem]" style={ { height: 'calc(100% - 21px)' } }>
                 {children}
             </div>
         </div>
--- src/components/blockRenderers/ButtonBlock.tsx
+++ src/components/blockRenderers/ButtonBlock.tsx
@@ -5,13 +5,15 @@ type ButtonBlockProps = {
     block: TButtonBlock;
 }
 
-const ButtonBlock: React.FC<ButtonBlockProps> = ({ block }) => {
-
-    return (
-        <div>
-            this is the button block renderer
-        </div>
-    );
-};
+const ButtonBlock: React.FC<ButtonBlockProps> = ({ block }) => (
+    <div className='m-2 flex justify-center p-6'>
+        <button
+            style={{ backgroundColor: block.bgColor, color: block.color }}
+            className='pl-4 pr-4 pt-2 pb-2 rounded-lg'
+        >
+            { block.text}
+        </button>
+    </div>
+);
 
 export default ButtonBlock;
