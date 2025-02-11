02d08bbeee6b1546553641b37edf085ba7e72273
Antonio Cosentino <hello@antoniocosentino.it>
2025-02-11 11:05:12 +0100

    rainbow footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+    <footer className="absolute bottom-0 left-0 w-full p-0 h-2 [background-image:linear-gradient(90deg,#16c0fd,#af48ed_50%,#ff8933)]"></footer>
+);
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
 import Header from '../components/Header';
 import { AppContextProvider } from '../context/AppContext';
 import MainView from '../components/MainView';
+import Footer from '../components/Footer';
 
 export default function Home() {
 
     return (
         <AppContextProvider>
-            <div className="relative min-h-screen bg-gray-100">
+            <div className="relative min-h-screen flex flex-col bg-gray-100">
                 <Header />
 
-                <main className="flex justify-center items-center min-h-screen pt-16">
+                <main className="flex-grow flex justify-center items-center pt-16 pb-4">
                     <MainView />
                 </main>
+
+                <Footer />
             </div>
         </AppContextProvider>
-
     );
 }
